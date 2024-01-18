004dae3a14960ada7a66fe60a9a87d2bfe068ab8
mcsouh <maxime.herbiet@gmail.com>
2024-01-18 16:37:43 +0100

    Refactor Utils

--- index.php
+++ index.php
@@ -6,7 +6,9 @@ require_once './bootstrap.php';
 
 use App\Utils\NewsManager;
 
-foreach (NewsManager::getInstance()->listNews() as $news) {
+$newsManager = new NewsManager();
+
+foreach ($newsManager->listNews() as $news) {
 	echo("############ NEWS " . $news->title . " ############\n");
 	echo($news->body . "\n");
 	foreach ($news->comments as $comment) {
