bf8d8043ffaa9e07ee39d2b9147a862dc68fe198
mcsouh <maxime.herbiet@gmail.com>
2024-01-18 16:59:08 +0100

    Sensitive credentials configuration

--- bootstrap.php
+++ bootstrap.php
@@ -5,13 +5,24 @@ require_once './autoload.php';
 
 use Illuminate\Database\Capsule\Manager as Capsule;
 
+$config = include 'config.php';
+
+$dbDriver = $config['DB_DRIVER'];
+$dbHost = $config['DB_HOST'];
+$dbPort = $config['DB_PORT'];
+$dbDatabase = $config['DB_DATABASE'];
+$dbUsername = $config['DB_USERNAME'];
+$dbPassword = $config['DB_PASSWORD'];
+
+
 $capsule = new Capsule;
 $capsule->addConnection([
-   "driver" => "mysql",
-   "host" =>"127.0.0.1:8889",
-   "database" => "phptest",
-   "username" => "root",
-   "password" => "root"
+   "driver" => $dbDriver,
+   "host" => $dbHost,
+   "port" => $dbPort,
+   "database" => $dbDatabase,
+   "username" => $dbUsername,
+   "password" => $dbPassword,
 ]);
 $capsule->setAsGlobal();
 $capsule->bootEloquent();
---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'DB_DRIVER' => 'mysql',
+    'DB_HOST' => '127.0.0.1',
+    'DB_PORT' => 8889,
+    'DB_DATABASE' => 'phptest',
+    'DB_USERNAME' => 'root',
+    'DB_PASSWORD' => 'root',
+];
